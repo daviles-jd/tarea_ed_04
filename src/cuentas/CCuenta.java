d4c8da3bca19bddc16c35a9b7b4392616766b56a
daviles-jd <nicogarcia780@gmail.com>
2023-02-14 15:57:46 +0100

    código refactorizado y con la documentación generada con javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,5 +1,9 @@
 package cuentas;
 
+/**
+ * Clase para gestionar cuenta bancaria
+ * @author david paz
+ */
 public class CCuenta {
     
     private String nombre;
@@ -63,11 +67,20 @@ public class CCuenta {
         this.tipoInteres = tipoInteres;
     }
 
-
+    /**
+     * Constructor
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -75,18 +88,32 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * 
+     * @return Saldo
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Método ingresar
+     * @param cantidad
+     * @throws Exception 
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Método retirar
+     * @param cantidad
+     * @throws Exception 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
